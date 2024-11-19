23f876c9ce85a3dd8590f604a72eea14d6487661
mariusz.niedzielski <mariusz.niedzielski@gamp.pl>
2024-11-19 13:38:25 +0100

    project details section fix.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProjectFormComponent } from './project-form/project-form.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ProjectFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project1';
 }
---
+++ src/app/project-form/project-form.component.ts
@@ -0,0 +1,82 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export interface Member {
+  id: number;
+  name: string;
+  role: string;
+  availability: boolean;
+  isAvailable?: Date;
+}
+
+export interface Project {
+  name: string;
+  description: string;
+  teamMembers: Member[];
+}
+
+@Component({
+  selector: 'app-project-form',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './project-form.component.html',
+  styleUrl: './project-form.component.css',
+})
+export class ProjectFormComponent {
+  project: Project = {
+    name: '',
+    description: '',
+    teamMembers: [],
+  };
+
+  roles = ['Developer', 'Designer', 'Tester'];
+
+  projectName = '';
+  projectDescription = '';
+  newMemberName = '';
+  newMemberRole = '';
+  newMemberAvailability = false;
+
+  addProject() {
+    if (!this.projectName) {
+      alert('Project name is required!');
+    }
+    this.project.name = this.projectName;
+    this.project.description = this.projectDescription;
+    this.projectName = '';
+    this.projectDescription = '';
+  }
+
+  addMember() {
+    if (!this.newMemberName || !this.newMemberRole) {
+      alert('Name and role are required!');
+      return;
+    }
+
+    this.project.teamMembers.push({
+      id: Math.floor(Math.random() * 1000),
+      name: this.newMemberName,
+      role: this.newMemberRole,
+      availability: this.newMemberAvailability,
+    });
+
+    this.newMemberName = '';
+    this.newMemberRole = '';
+    this.newMemberAvailability = false;
+  }
+
+  removeMember(memberId: number) {
+    this.project.teamMembers = this.project.teamMembers.filter(
+      (member) => member.id !== memberId
+    );
+  }
+
+  clearMembers() {
+    this.project.teamMembers = [];
+  }
+
+  toggleAvailability(member: any) {
+member.isAvailable = member.availability ? new Date() : undefined
+  }
+}
